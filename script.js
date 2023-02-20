b1a8f9214d782a6ede39575f3f8f62847b037a66
bKe <indzicbakir@gmail.com>
2023-02-21 00:29:44 +0100

    big update

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const answer4 = document.getElementById("right2Button")
 const score = document.getElementById("score")
 const record = document.getElementById("record")
 const time = document.getElementById("time")
-const startbutton = document.getElementById("startButtom")
+const startbutton = document.getElementById("startbtn")
 const footer = document.getElementById("footer")
 const content = document.getElementById("content")
 const endp = document.getElementById("endParagraph")
@@ -15,176 +15,110 @@ const contentH2 = document.getElementById("contentTitle")
 const h1 = document.getElementById("h1")
 const body = document.getElementById("body")
 const startpara = document.getElementById("startpa  ")
-
+const nextbutton = document.getElementById("nextButtom")
 
 let currentQuestionIndex = 0;
 let currentScore = 0;
 
 const easyquestions = [
     pitanje1 = {
-        title: "What is the longest river in the world?",
-        correctAnswer: "Nil",
-        otherAnswers: ["Amazon", "Jang ce kjang", "Nil", "Mississippi"]
+        title: "Zamjenica هما znači ",
+        correctAnswer: "Njih dvojica",
+        otherAnswers: ["Njih dvojica", "On", "Mi", "Vas dvojica"]
+    },
+    pitanje1 = {
+        title: "Zamjenica أنا znači ",
+        correctAnswer: "Ja",
+        otherAnswers: ["Njih dvojica", "Ja", "Mi", "Njih dvije"]
     },
     pitanje2 = {
-        title: "What is the highest mountain in Bosnia?",
-        correctAnswer: "Maglić",
-        otherAnswers: ["Maglić", "Kozara", "Bjelašnica", "Vlašić"]
+        title: "Koliko ima harfova u arapskom jeziku?",
+        correctAnswer: "28",
+        otherAnswers: ["28", "27", "30", "31"]
+    },
+    pitanje14 = {
+        title: "Koliko ima vrsta glagola?",
+        correctAnswer: "10",
+        otherAnswers: ["10", "11", "12", "8"]
     },
     pitanje3 = {
-        title: "How many months have 28 days? ",
-        correctAnswer: "12",
-        otherAnswers: ["1", "2", " 6", "12"]
-    },
-    pitanje5 = {
-        title: "What color is the M in McDonald’s?",
-        correctAnswer: "Yellow",
-        otherAnswers: ["Yellow", "Red", "Blue", "Green"]
+        title: "Šta znači riječ - نقرأ ",
+        correctAnswer: "Mi čitamo",
+        otherAnswers: ["Mi čitamo", "Mi smo čitali", "Čitanje", "On čita"]
     },
     pitanje4 = {
-        title: "Area of the Earth is ____",
-        correctAnswer: "510.100.000 km²",
-        otherAnswers: ["40.100.000 km²", "410.100.000 km²", "510.000.000 km²", "510.100.000 km²"]
+        title: "Koja vrsta glagola je riječ  -  اِنْتَظَرْنَ?",
+        correctAnswer: "8.",
+        otherAnswers: ["8.", "10.", "9.", "7."]
+    },
+    pitanje4 = {
+        title: "Koja vrsta glagola je riječ  -  قَدَّمَ?",
+        correctAnswer: "2.",
+        otherAnswers: ["6.", "2.", "4.", "7."]
+    },
+    pitanje13 = {
+        title: "Jug se na arapskom kaže _____?",
+        correctAnswer: "جنوب",
+        otherAnswers: ["جنوب", "غرب", "شرق", "شمال"]
     },
     pitanje5 = {
-        title: "Most mentioned prophet in Qur'an is ____ ",
-        correctAnswer: "Moses",
-        otherAnswers: ["Salih", "Jesus", "Mohammed", "Moses"]
+        title: "Kako se kaže 'crven' na arapskom?",
+        correctAnswer: "أحمر",
+        otherAnswers: ["أحمر", "حمراء", "أزرق", "أسود"]
     },
     pitanje6 = {
-        title: "When did the World war start?",
-        correctAnswer: "1914.",
-        otherAnswers: ["1916.", "1914.", "1814.", "1924."]
+        title: "Koji od ovih pridjeva je u superlativu?",
+        correctAnswer: "الاُسْرَعَ",
+        otherAnswers: ["الاُسْرَعَ", "أَكْبَرُ مِنْ", "سَرِيعَ", "كَبِير"]
     },
     pitanje7 = {
-        title: "Whats 18th letter of the alphabet? ",
-        correctAnswer: "R",
-        otherAnswers: ["Q", "R", " O", "S"]
-    },
-    pitanje8 = {
-        title: "When did the Ottomans occupy BiH? ",
-        correctAnswer: "1463.",
-        otherAnswers: ["1463.", "1878.", "1436.", "1787."]
-    },
-    pitanje9 = {
-        title: "What city is known as the City of Love? ",
-        correctAnswer: "Paris",
-        otherAnswers: ["Berlin", "Paris", " Madrid", "Rome"]
+        title: "Koje zanimanje je حَكَم?",
+        correctAnswer: "Sudija",
+        otherAnswers: ["Sudija", "Golman", "Igrač", "Komentator"]
     },
     pitanje10 = {
-        title: "Who is the founder of Amazon?",
-        correctAnswer: "Jeff Bezos",
-        otherAnswers: ["Jeff Bezos", "Elon Musk", "Bill Gates", "Mark Zuckerberg"]
+        title: "Koji od ovih glagola je nakis?",
+        correctAnswer: "مشى",
+        otherAnswers: ["قرأ", "مشى", "قال", "وجد"]
     },
-    pitanje_1 = {
-        title: "What gets wetter the more it dries? ",
-        correctAnswer: "Towel",
-        otherAnswers: ["Ocean", "Sponge", "Person", "Towel"]
+    pitanje11 = {
+        title: "Imenica se sa pridjevom slaže u...",
+        correctAnswer: "Rodu, broju, padežu i određenosti",
+        otherAnswers: ["Rodu, broju, padežu i određenosti", "Rodu i broju", "Padežu", "Ničemu"]
     },
-    pitanje2 = {
-        title: "What is the capital of Somalia? ",
-        correctAnswer: "Mogadish",
-        otherAnswers: ["Mogadish", "Kinshasa", " Adis Abeba", "Asmara"]
-    },
-    pitanje3 = {
-        title: "What is average brain weight? ",
-        correctAnswer: "1,25-1,5",
-        otherAnswers: ["1,25-1,5", "1,1-1,7", "1-2", "2,1-2,5"]
-    },
-    pitanje4 = {
-        title: "Solve: 6 / 2 (1 + 2) = ?",
-        correctAnswer: "9",
-        otherAnswers: ["1", "7", "9", "3"]
-    },
-    pitanje5 = {
-        title: "How many stars are on the national flag of USA?",
-        correctAnswer: "50",
-        otherAnswers: ["7", "50", "48", "51"]
-    },
-    pitanje6 = {
-        title: "How many time zones are there in Russia?",
-        correctAnswer: "11",
-        otherAnswers: ["11", "9", "3", "19"]
-    },
-    pitanje7 = {
-        title: "Who led an expedition to prove that the Earth is round?",
-        correctAnswer: "Ferdinand Magellan",
-        otherAnswers: ["Ferdinand Magellan", "Vascp da Gama", "Christopher Columbus", "Amerigo Vespucci"]
+    pitanje12 = {
+        title: "Kako se na arapskom kaže 25?",
+        correctAnswer: "خمسة وعشرون",
+        otherAnswers: ["خمسة وعشرون", "اثنان وخمسون", "اثني عشر", "خمسمائة وعشرون"]
     },
     pitanje8 = {
-        title: "What year was Adolf Hitler born?",
-        correctAnswer: "1889.",
-        otherAnswers: ["1889.", "1900.", "1884.", "1869."]
+        title: "كِتَابُ زَيْنَب",
+        correctAnswer: "كِتَابُ زَيْنَبَ",
+        otherAnswers: ["كِتَابُ زَيْنَبَ", "كِتَابُ زَيْنَبِ", "كِتَابُ زَيْنَبُ", "كِتَابُ زَيْنَبٍ"]
     },
     pitanje9 = {
-        title: "How many possible combinations does a rubic's cube have? ",
-        correctAnswer: "43 quintillion",
-        otherAnswers: ["43 quintillion", "38 billion", "728 million", "12 quintillion"]
+        title: "إنّ هذا ـــــــــ جديدٌ",
+        correctAnswer: "الطالبَ",
+        otherAnswers: ["الطالبَ", "الطالبِ", "الطالبًا", "الطالبُ"]
     },
-    pitanje__10 = {
-        title: "π is 3.14",
-        correctAnswer: "1592",
-        otherAnswers: ["1518", "1592", "1582", "1581"]
-    },
-    pitanje_1 = {
-        title: "Chess was made in ____",
-        correctAnswer: "6. century",
-        otherAnswers: ["9. century", "6. century", "2. century", "10. century"]
-    },
-    pitanje2 = {
-        title: "Biggest animal in water is ____",
-        correctAnswer: "Lion’s Mane Jellyfish",
-        otherAnswers: ["Giant Pacific Octopus", "Lion’s Mane Jellyfish", "Antarctic blue whale", "Whale Shark"]
-    },
-    pitanje3 = {
-        title: "Who won fifth ever Fifa world cup? ",
-        correctAnswer: "Germany",
-        otherAnswers: ["Germany", "Sweden", " Switzerland", "Brazil"]
-    },
-    pitanje4 = {
-        title: "What was first sport ever? ",
-        correctAnswer: "Wrestling",
-        otherAnswers: ["Wrestling", "Running", "  Swimming", "Archery"]
-    },
-
-    pitanje6 = {
-        title: "Song informer was released in ____ ",
-        correctAnswer: "1993.",
-        otherAnswers: ["2001.", "1997.", "1978.", "1993."]
-    },
-    pitanje7 = {
-        title: "Who designed the Eiffel Tower? ",
-        correctAnswer: "Gustave Eiffel",
-        otherAnswers: ["Gustave Eiffel", "Albert Eiffel", "Edouard Eiffel", "Alexandre Bonickhausen dit Eiffel"]
-    },
-    pitanje8 = {
-        title: "As of December 2021, what's the highest rated film on iMDB?",
-        correctAnswer: "The Shawshank Redemption",
-        otherAnswers: ["The Shawshank Redemption", "Avatar", "Spider-Man: No Way Home", "The Dark Knight"]
-    },
-    pitanje9 = {
-        title: "What is Obamas middle name?",
-        correctAnswer: "Hussein",
-        otherAnswers: ["Hussein", "James", "William", "	Thomas"]
-    },
-    pitanje_10 = {
-        title: "Who said: 'Tell me and I forget. Teach me and I remember. Involve me and I learn.' ",
-        correctAnswer: "Benjamin Franklin",
-        otherAnswers: ["Benjamin Franklin", "Anne Frank", "Abraham Lincoln", "Albert Einstein"]
+    pitanje13 = {
+        title: "كلبه znači...",
+        correctAnswer: "Njegov pas",
+        otherAnswers: ["Njegov pas", "Njen pas", "Moj pas", "Naš pas"]
     },
 ]
 
 
 function setUp() {
     score.innerHTML = 0
-    time.innerHTML = "90 seconds remaining"
+    currentScore = 0
 }
 setUp()
 
 //easy-------------------------------------------------------
 
 function startbtn() {
-    body.style.background = "linear-gradient(90deg, hsla(120, 75%, 31%, 1) 0%, hsla(181, 94%, 25%, 1) 48%, hsla(209, 70%, 39%, 1) 100%)"
+    body.style.background = "linear-gradient(90deg, rgb(16, 118, 235) 0%, rgb(19, 211, 92) 100%)"
 
     let niz = [];
     for (let i = 0; i < 4; i++) {
@@ -199,12 +133,12 @@ function startbtn() {
 
     h1.style.marginTop = "10%"
     answer1.style.visibility = "visible"
+    nextbutton.style.visibility = "visible"
     answer2.style.visibility = "visible"
     answer3.style.visibility = "visible"
     answer4.style.visibility = "visible"
     footer.style.visibility = "visible"
-    document.getElementById("startpa").remove()
-    document.getElementById("startButtom").remove()
+    document.getElementById("startbtn").remove()
     const question = easyquestions[currentQuestionIndex]
     pitanje.innerHTML = question.title
     const answers = question.otherAnswers
@@ -213,19 +147,6 @@ function startbtn() {
     answer3.innerHTML = answers[niz[2]]
     answer4.innerHTML = answers[niz[3]]
 
-    var timeLeft = 90;
-    var elem = document.getElementById("time");
-    var timerId = setInterval(countdown, 1000);
-    function countdown() {
-        if (timeLeft == -1) {
-            clearTimeout(timerId);
-            endTimeTitle();
-        }
-        else {
-            elem.innerHTML = timeLeft + ' seconds remaining';
-            timeLeft--;
-        }
-    }
     function buttonClick(event) {
         let niz = [];
         for (let i = 0; i < 4; i++) {
@@ -239,90 +160,55 @@ function startbtn() {
         }
         const answer = event.target.innerHTML
         const question = easyquestions[currentQuestionIndex]
-        if (question.correctAnswer == "Jeff Bezos") {
-            setTimeout(() => {
-                console.log(1)
-                body.style.background = "linear-gradient(90deg, hsla(26, 67%, 50%, 1) 0%, hsla(71, 94%, 25%, 1) 48%, hsla(112, 74%, 30%, 1) 100%)"
-            }, 1000)
+        function nextq() {
+            currentQuestionIndex = currentScore
+            if (currentQuestionIndex == easyquestions.length) {
+                endTimeTitle()
+            }
 
-        }
-        if (question.correctAnswer == "1592") {
-            setTimeout(() => {
-                body.style.background = "linear-gradient(90deg, hsla(2, 87%, 46%, 1) 0%, hsla(23, 74%, 46%, 1) 50%, hsla(50, 74%, 30%, 1) 100%)"
-            }, 1000)
-
-        }
-        if (answer === pitanje_10.correctAnswer) {
-            score.innerHTML = currentScore += 1
-            endTimeTitle()
-        }
-        if (answer === pitanje_10.otherAnswers) {
+            const newQuestion = easyquestions[currentQuestionIndex]
+            pitanje.innerHTML = newQuestion.title
+            const answers = newQuestion.otherAnswers
+            answer1.innerHTML = answers[niz[0]]
+            answer2.innerHTML = answers[niz[1]]
+            answer3.innerHTML = answers[niz[2]]
+            answer4.innerHTML = answers[niz[3]]
             score.innerHTML = currentScore
-            endTimeTitle()
+            score2 = score.innerHTML
+            event.target.style.boxShadow = "none";
         }
-
         if (answer === question.correctAnswer) {
             event.target.style.boxShadow = "1px 1px 66px 8px rgba(16,237,53,0.82)";
             currentQuestionIndex += 1
-            const newQuestion = easyquestions[currentQuestionIndex]
-            setTimeout(() => {
-                pitanje.innerHTML = newQuestion.title
-                const answers = newQuestion.otherAnswers
-                answer1.innerHTML = answers[niz[0]]
-                answer2.innerHTML = answers[niz[1]]
-                answer3.innerHTML = answers[niz[2]]
-                answer4.innerHTML = answers[niz[3]]
-                score.innerHTML = currentScore += 1
-                score2 = score.innerHTML
-                event.target.style.boxShadow = "none";
-            }, 1000)
-
+            currentScore += 1
+            score.innerHTML = currentScore
+            nextbutton.addEventListener("click", nextq)
         }
-
-
         else {
             event.target.style.boxShadow = "1px 1px 66px 8px rgba(255, 51, 51)";
-            currentQuestionIndex += 1
-            const newQuestion = easyquestions[currentQuestionIndex]
             setTimeout(() => {
-                pitanje.innerHTML = newQuestion.title
-                const answers = newQuestion.otherAnswers
-                answer1.innerHTML = answers[niz[0]]
-                answer2.innerHTML = answers[niz[1]]
-                answer3.innerHTML = answers[niz[2]]
-                answer4.innerHTML = answers[niz[3]]
-                score.innerHTML = currentScore
-                score2 = score.innerHTML
-                event.target.style.boxShadow = "none";
+                event.target.style.boxShadow = "";
             }, 1000)
         }
     }
 
     function endTimeTitle() {
-        pitanje.style.visibility = "hidden"
-        answer1.style.visibility = "hidden"
-        answer2.style.visibility = "hidden"
-        answer3.style.visibility = "hidden"
-        answer4.style.visibility = "hidden"
+        document.getElementById("question").remove()
+        document.getElementById("leftButton").remove()
+        document.getElementById("rightButton").remove()
+        document.getElementById("left2Button").remove()
+        document.getElementById("right2Button").remove()
         footer.style.visibility = "hidden"
+        nextbutton.style.visibility = "hidden"
         content.style.visibility = "visible"
 
-        contentH2.innerHTML = "Results"
+        contentH2.innerHTML = "Rezultati"
         h1.style.marginTop = "15%"
-        parText = ["You scored", score.innerHTML, "/", easyquestions.length, "."]
+        parText = ["Tačno si odgovorio na ", score.innerHTML, "/", easyquestions.length, "pitanja."]
         endp.innerHTML = parText.join(' ')
-        if (score.innerHTML < 11) {
-            timeText = ["Your intellingence is sadly below average :("]
-            timep.innerHTML = timeText.join(' ')
-        }
-        else if (score.innerHTML < 21) {
-            timeText = ["Your intelligence is average. Good for you.."]
-            timep.innerHTML = timeText.join(' ')
-        }
-        else {
-            timeText = ["AMAZING... You have above average intelligence!!!"]
-            timep.innerHTML = timeText.join(' ')
-        }
+        timeText = ["Završili ste Qviz. Nadam se da je bilo zanimljivo :)"]
+        timep.innerHTML = timeText.join(' ')
+
         setTimeout(() => {
             window.location.reload()
         }, 100000)
@@ -333,21 +219,3 @@ function startbtn() {
     answer4.addEventListener("click", buttonClick)
 }
 startbutton.addEventListener("click", startbtn)
-
-//End------------------------------------------------------------------------------------------
-function wrongAnswer() {
-    currentQuestionIndex += 1
-    const newQuestion = easyquestions[currentQuestionIndex]
-    setTimeout(() => {
-        pitanje.innerHTML = newQuestion.title
-        const answers = newQuestion.otherAnswers
-        answer1.innerHTML = answers[niz[0]]
-        answer2.innerHTML = answers[niz[1]]
-        answer3.innerHTML = answers[niz[2]]
-        answer4.innerHTML = answers[niz[3]]
-        score.innerHTML = currentScore
-        score2 = score.innerHTML
-        event.target.style.boxShadow = "none";
-    }, 1000)
-
-}
